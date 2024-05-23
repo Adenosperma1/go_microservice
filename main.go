570d1653b84c54e7a068a77476c7d72890c7d6f0
Brendan Wilde <44901073+Adenosperma1@users.noreply.github.com>
2024-05-23 17:27:06 +1000

    basic http handler for a server

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
 func main(){
+
+
+//handlers
+http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request){
+	log.Println("hi")
+	d, err := io.ReadAll(r.Body)
+	if err != nil{
+		http.Error(rw, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("Data______________ %s", d)
+})
+
+http.HandleFunc("/bye", func(w http.ResponseWriter, r *http.Request){
+	log.Println("bye")
+})
 	
+
+
+http.ListenAndServe(":9090", nil) // this will run in the browser localhost:9090
+
 }
+
